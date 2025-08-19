4f1dfb8577b91ca797cd759067578fd66c7128c5
Orion Raysher Lee <orionraysher65@gmail.com>
2025-08-20 00:52:33 +0800

    Add Task class
    Each object in Task have a description that describes the task
    
    Scribbles, as a Task list manager / chatbot would be required to manage
    a list of tasks, having a task class would be essential to isolate what information
    and functionalities a task should have and how Scribbles interact with Task

---
+++ src/main/java/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    private final String desc;
+
+    public Task(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+}
