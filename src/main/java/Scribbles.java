1505ff787af0ee0c1cd70e6c775c63fa2c152371
Orion <orionraysher65@gmail.com>
2025-08-19 18:06:36 +0800

    Modify Duke.java to Scribbles.java
    Modify template for Project Duke to Scribbles, a chatbot
    with a more fun and quirky personality to differentiate
    the project from other people using the same template

---
+++ src/main/java/Scribbles.java
@@ -0,0 +1,17 @@
+public class Scribbles {
+    public static void main(String[] args) {
+        String welcomeMsg = """
+              .----------------------------------------------------.
+              | Heya! I'm Scribbles, your personal task assistant! |
+              | What can I do for you? :)                          |
+              '----------------------------------------------------'
+              """;
+        String exitMsg = """
+              .----------------------------------------------------.
+              | Bai bai! See you next time! :D                     |
+              '----------------------------------------------------'
+              """;
+        System.out.println(welcomeMsg);
+        System.out.println(exitMsg);
+    }
+}
