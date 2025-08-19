c46baa2449566db5d0ab4aafef93bc9a5419203c
Orion Raysher Lee <orionraysher65@gmail.com>
2025-08-20 01:32:06 +0800

    Add marking feature and toString for Task class
    Task now have a new field 'isDone' to mark and unmark task as done
    
    toString() is added for future purposes where more details are added into Task,
    e.g. deadlines, categories, etc. which all can be incorporated to how task is being
    displayed on Scribbles

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,31 @@
 public class Task {
     private final String desc;
+    private boolean isDone;
 
     public Task(String desc) {
         this.desc = desc;
+        this.isDone = false;
     }
 
     public String getDesc() {
         return this.desc;
+    }
+
+    // Reused and inspired from Partial Solution provided for Level-3 requirements
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");  // mark done task with X
+    }
+
+    public void mark() {
+        this.isDone = true;
+    }
+
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return "%s %s".formatted(getStatusIcon(), this.desc);
     }
 }
