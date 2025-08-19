3e0acb93cce6b82f2fc53092182f0d4ce675f62e
Orion <orionraysher65@gmail.com>
2025-08-20 00:00:12 +0800

    Add in echo() in Scribbles and logic flow in main()
    Scribbles can interact with users by printing out whatever the
    user types. When the user types 'bye' (case-insensitve), Scribbles
    will terminate with an exit message.

--- src/main/java/Scribbles.java
+++ src/main/java/Scribbles.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Scribbles {
 
     public static final String WELCOMEMSG = """
@@ -11,8 +14,30 @@ public class Scribbles {
             | Bai bai! See you next time! :D                     |
             '----------------------------------------------------'
             """;
+
+    public void echo(String cmd) {
+        String output = """
+                    <----------------------------------------------->
+                    %s
+                    <----------------------------------------------->
+                """.formatted(cmd);
+        System.out.println(output);
     }
 
     public static void main(String[] args) {
+        System.out.println(Scribbles.WELCOMEMSG);
+        Scribbles scribble = new Scribbles();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+
+        String command = scanner.nextLine();
+        while (!command.equalsIgnoreCase("bye")) {
+            scribble.echo(command);
+            System.out.print("> ");
+            command = scanner.nextLine();
+        }
+
+        System.out.println(Scribbles.EXITMSG);
+
     }
 }
