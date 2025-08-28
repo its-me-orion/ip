4143a5e0f01a5fb06c6d044f7f99de0eede9909e
Orion Raysher Lee <orionraysher65@gmail.com>
2025-08-28 20:50:52 +0800

    Add new Exceptions for Scribbles

--- src/main/java/scribbles/exception/MissingArgumentException.java
+++ src/main/java/scribbles/exception/MissingArgumentException.java
@@ -2,7 +2,6 @@ package scribbles.exception;
 
 public class MissingArgumentException extends ScribblesException {
     public MissingArgumentException(String... params) {
-        super("Error: You did not provide any arguments for the following parameter(s): %s (._.)"
-                .formatted(String.join(", ", params)));
+        super("Error: Your command is missing certain arguments...");
     }
 }
