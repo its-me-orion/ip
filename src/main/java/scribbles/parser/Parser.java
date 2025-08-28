b5ebf8ea5ca1c2c2f3468cfc54843a1539fb8694
Orion Raysher Lee <orionraysher65@gmail.com>
2025-08-28 20:51:38 +0800

    Extract Parser out of Scribbles

---
+++ src/main/java/scribbles/parser/Parser.java
@@ -0,0 +1,142 @@
+package scribbles.parser;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import scribbles.command.AddDeadlineCommand;
+import scribbles.command.AddEventCommand;
+import scribbles.command.AddToDoCommand;
+import scribbles.command.Command;
+import scribbles.command.DeleteCommand;
+import scribbles.command.ExitCommand;
+import scribbles.command.ListCommand;
+import scribbles.command.MarkCommand;
+import scribbles.command.UnknownCommand;
+import scribbles.command.UnmarkCommand;
+import scribbles.exception.InvalidDateTimeException;
+import scribbles.exception.InvalidParamException;
+import scribbles.exception.MissingArgumentException;
+import scribbles.exception.MissingDescriptionException;
+import scribbles.exception.ScribblesException;
+import scribbles.exception.WrongParamOrderException;
+
+public class Parser {
+
+    public static final DateTimeFormatter INPUT_FORMAT = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+    public static final DateTimeFormatter PRINT_FORMAT = DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma");
+
+    public static Command parseCommand(String userCommand) throws ScribblesException {
+        String[] tokens = userCommand.trim().split(" ", 2);
+        String cmd = tokens[0].toLowerCase();
+        String args = tokens.length > 1 ? tokens[1] : "";
+
+        switch (cmd) {
+        case "bye":
+            return new ExitCommand();
+        case "list":
+            return new ListCommand();
+        case "mark":
+            return parseMark(args);
+        case "unmark":
+            return parseUnmark(args);
+        case "delete":
+            return parseDelete(args);
+        case "todo":
+            return parseToDo(args);
+        case "deadline":
+            return parseDeadline(args);
+        case "event":
+            return parseEvent(args);
+        default:
+            return new UnknownCommand(cmd);
+        }
+    }
+
+    private static Command parseMark(String args) throws ScribblesException {
+        int index = Parser.parseIndex(args);
+        return new MarkCommand(index);
+    }
+
+    private static Command parseUnmark(String args) throws ScribblesException {
+        int index = Parser.parseIndex(args);
+        return new UnmarkCommand(index);
+    }
+
+    private static Command parseDelete(String args) throws ScribblesException {
+        int index = Parser.parseIndex(args);
+        return new DeleteCommand(index);
+    }
+
+    private static int parseIndex(String args) throws ScribblesException {
+        if (args.isEmpty()) {
+            throw new MissingArgumentException();
+        }
+
+        try {
+            int index = Integer.parseInt(args.trim());
+            if (index <= 0) {
+                throw new ScribblesException("Index cannot be negative!!!");
+            }
+            return index - 1;
+        } catch (NumberFormatException e) {
+            throw new ScribblesException("Argument needs to be an integer :/");
+        }
+    }
+
+    private static Command parseToDo(String args) throws ScribblesException {
+        if (args.isEmpty()) {
+            throw new MissingDescriptionException();
+        }
+        return new AddToDoCommand(args);
+    }
+
+    private static Command parseDeadline(String args) throws ScribblesException {
+        if (!args.contains(" /by ")) {
+            throw new InvalidParamException("/by");
+        }
+
+        String[] descAndBy = args.split(" /by ", 2);
+        if (descAndBy.length < 2 || descAndBy[0].trim().isEmpty()) {
+            throw new MissingDescriptionException();
+        }
+
+        String desc = descAndBy[0];
+        LocalDateTime by = Parser.parseDateTime(descAndBy[1]);
+        return new AddDeadlineCommand(desc, by);
+    }
+
+    private static Command parseEvent(String args) throws ScribblesException {
+        if (!args.contains(" /from ") || !args.contains(" /to ")) {
+            throw new InvalidParamException("/from", "/to");
+        }
+
+        if (args.indexOf(" /from ") > args.indexOf(" /to ")) {
+            throw new WrongParamOrderException("/from", "/to");
+        }
+
+        String[] descFromTo = args.split(" /from ", 2);
+        if (descFromTo[0].trim().isEmpty()) {
+            throw new MissingDescriptionException();
+        }
+        String desc = descFromTo[0];
+
+        String[] fromTo = descFromTo[1].split(" /to ", 2);
+        if (fromTo.length < 2) {
+            throw new MissingArgumentException();
+        }
+
+        LocalDateTime from = Parser.parseDateTime(fromTo[0]);
+        LocalDateTime to = Parser.parseDateTime(fromTo[1]);
+
+        return new AddEventCommand(desc, from, to);
+    }
+
+    public static LocalDateTime parseDateTime(String dateTime) throws ScribblesException {
+        try {
+            return LocalDateTime.parse(dateTime, INPUT_FORMAT);
+        } catch (DateTimeParseException e) {
+            throw new InvalidDateTimeException();
+        }
+    }
+}
